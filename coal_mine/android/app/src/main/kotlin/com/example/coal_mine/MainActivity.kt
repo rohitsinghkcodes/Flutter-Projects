15a490ae7f60d07b48010eb8c581e1bd9a7979bf
Rohit Singh <conankmails@gmail.com>
2020-08-16 03:32:43 +0530

    Hitesh Tutorial project added + made some changes in excisting files

--- coal_mine/android/app/src/main/kotlin/com/example/coal_mine/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coal_mine
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- quizzler/lib/questions.dart
+++
@@ -1,8 +0,0 @@
-class Questions {
-  String question;
-  bool answer;
-  Questions({String q, bool a}) {
-    question = q;
-    answer = a;
-  }
-}
